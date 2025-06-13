f080711e5412d3b1789ed0f365c8380d8807385b
Ajioz <sunny.ajiroghene@gmail.com>
2025-06-13 09:09:39 +0100

    added more context

--- basic/autoencoder_v1.py
+++ basic/autoencoder_v1.py
@@ -1,18 +1,29 @@
-from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
-from tensorflow.keras.models import Sequential
 import matplotlib.pyplot as imshow
 import numpy as np
 import cv2
-from keras.preprocessing.image import img_to_array, load_img
-# Define the size of the input images
 
-np.random.seed(42)  # For reproducibility
+from keras.preprocessing.image import img_to_array
+from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
+from tensorflow.keras.models import Sequential
+
+# Set random seed for reproducibility
+np.random.seed(42)  
 
 SIZE = 256
 
 img_data = []
 
-img = cv2.imread('images/monalisa.jpg')  # Load an image from the specified path
+img = cv2.imread('images/monalisa.jpg', 1)  # Load an image from the specified path as colored hence --> 1
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convert the image from BGR to RGB format
+img = cv2.resize(img, (SIZE, SIZE))  # Resize the image to the specified size
+img_data.append(img_to_array(img))  # Append the image to the list
+
+img_array = np.reshape(img_data,(len(img_data), SIZE, SIZE, 3))  # Reshape the list to a numpy array
+img_array = img_array.astype('float32') / 255.0  # Normalize the pixel values to the range [0, 1]
+
+
+
+
 # Initialize the Sequential model
 model = Sequential()
 
@@ -36,3 +47,9 @@ model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
 
 # Display the model summary
 model.summary()
+
+model.fit(img_array, img_array, epochs=5, batch_size=1, shuffle=True)
+
+predicted = model.predict(img_array)
+
+imshow.imshow(predicted[0].reshape(SIZE, SIZE, 3))  # Display the original image
