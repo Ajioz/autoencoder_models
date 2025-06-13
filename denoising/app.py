b2f21e80e3aaacc03d15303ed65dcb9c9295ec5a
Ajioz <sunny.ajiroghene@gmail.com>
2025-06-13 15:10:38 +0100

    added denoising

---
+++ denoising/app.py
@@ -0,0 +1,85 @@
+import os
+import matplotlib.pyplot as plt  # Changed alias from 'imshow' to 'plt'
+import numpy as np
+
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, UpSampling2D
+from tensorflow.keras.models import Sequential
+
+#loading the data
+(x_train, _), (x_test, _) = mnist.load_data()
+
+#normalizing the data
+x_train = x_train.astype('float32') / 255.  #normalizing the data
+x_test = x_test.astype('float32') / 255.    #normalizing the data
+
+#reshaping the data to 4D array
+x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))
+x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))
+
+SIZE = 28
+
+#adding some noise
+noise_factor = 0.5
+x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape) 
+x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape) 
+
+#clipping all values between 0 and 1
+x_train_noisy = np.clip(x_train_noisy, 0., 1.)
+x_test_noisy = np.clip(x_test_noisy, 0., 1.)
+
+#Displaying images with noise
+plt.figure(figsize=(20, 2))
+for i in range(1,10):
+    ax = plt.subplot(1, 10, i)
+    plt.imshow(x_test_noisy[i].reshape(28, 28), cmap="binary")
+plt.show()
+
+# Define autoencoder model
+model = Sequential()
+#Encoder
+model.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(28, 28, 1)))
+model.add(MaxPooling2D((2, 2), padding='same'))
+model.add(Conv2D(8, (3, 3), activation='relu', padding='same'))
+model.add(MaxPooling2D((2, 2), padding='same'))
+model.add(Conv2D(8, (3, 3), activation='relu', padding='same'))
+model.add(MaxPooling2D((2, 2), padding='same'))
+#Decoder
+model.add(Conv2D(8, (3, 3), activation='relu', padding='same'))
+model.add(UpSampling2D((2, 2)))
+model.add(Conv2D(8, (3, 3), activation='relu', padding='same'))
+model.add(UpSampling2D((2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(UpSampling2D((2, 2)))
+model.add(Conv2D(1, (3, 3), activation='relu', padding='same'))
+
+#Compiling the model
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+#Summarizing the model
+model.summary()
+
+model.fit(x_train_noisy, x_train, epochs=10, batch_size=256, shuffle=True, 
+          validation_data=(x_test_noisy, x_test))
+
+
+
+model.evaluate(x_test_noisy, x_test)
+
+model.save('denoising_autoencoder.model')
+
+no_noise_img = model.predict(x_test_noisy)
+
+plt.figure(figsize=(40, 4))
+for i in range(10):
+    # display original
+    ax = plt.subplot(3, 20, i + 1)
+    plt.imshow(x_test_noisy[i].reshape(28, 28), cmap="binary")
+    
+    # display reconstructed (after noise removed) image
+    ax = plt.subplot(3, 20, 40 +i+ 1)
+    plt.imshow(no_noise_img[i].reshape(28, 28), cmap="binary")
+
+plt.show()
+
+
